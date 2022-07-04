e70e364c5390eb14755ff0c0441d17e7830af681
gyh <gyhguanyonghua@163.com>
2022-07-04 08:36:33 +0800

    minor test

--- src/core/test/src/string/test_StringView.cpp
+++ src/core/test/src/string/test_StringView.cpp
@@ -18,15 +18,15 @@ public:
 
 	void test_1() {
 		if (f1() == f2()) {
-			SGE_DUMP_VAR("yes");
+			SGE_DUMP_VAR("f1 == f2"); // error
 		}
 
 		if (f2() == f3()) {
-			SGE_DUMP_VAR("yes");
+			SGE_DUMP_VAR("f2 == f3"); // error
 		}
-		
+
 		if (f1() == f3()) {
-			SGE_DUMP_VAR("yes");
+			SGE_DUMP_VAR("f1 == f3"); // error
 		}
 	}
 
@@ -78,11 +78,36 @@ public:
 			SGE_LOG("vi={} vt={} vn={}", vi, vt, vn);
 		}
 	}
+
+	template<size_t N>	static void foo(String_<N>& s)	{ SGE_LOG("foo String_: {}", s); }
+	template<size_t N>	static void foo(StringW_<N>& s) { SGE_LOG("foo StringW_: {}", s); }
+						static void foo(String& s)		{ SGE_LOG("foo String: {}", s); }
+						static void foo(StringW& s)		{ SGE_LOG("foo StringW: {}", s); }
+
+	void test_4() {
+		String s1 = "s1 - String";
+		foo(s1);
+
+		TempString s2 = "s2 - TempString";
+		foo(s2);
+
+		StringW s3 = L"s3 - StringW";
+		foo(s3);
+
+		String_<1> s4 = "s4 - String_1";
+		foo(s4);
+
+		StringW_<1> s5 = L"s5 - StringW_1";
+		foo(s5);
+	}
 };
 
 } // namespace 
 
 void test_StringView() {
 	using namespace sge;
-	SGE_TEST_CASE(Test_StringView, test_3());
+	//SGE_TEST_CASE(Test_StringView, test_1());
+	//SGE_TEST_CASE(Test_StringView, test_2());
+	//SGE_TEST_CASE(Test_StringView, test_3());
+	SGE_TEST_CASE(Test_StringView, test_4());
 }
