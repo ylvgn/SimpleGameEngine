a299e3eec1c483d85346eefa5b625f8cd18c3cba
gyh <gyhguanyonghua@163.com>
2023-05-17 11:22:43 +0800

    <Hands-On C++ Game Animation Programming> Chapter02 vec3 -> TVec3

--- src/books/Hands-On C++ Game Animation Programming/src/vec3.cpp
+++ src/books/Hands-On C++ Game Animation Programming/src/vec3.cpp
@@ -1,1 +1,11 @@
 #include "vec3.h"
+
+namespace sge {
+
+// explicit specialization to force VisualC check syntax in function body
+
+template TVec3<float>;
+template TVec3<int>;
+template TVec3<unsigned int>;
+
+}
--- src/books/Hands-On C++ Game Animation Programming/src/vec3.h
+++ src/books/Hands-On C++ Game Animation Programming/src/vec3.h
@@ -1,155 +1,198 @@
 #pragma once
 
+#include "vec2.h"
+
 namespace sge {
 
 //FYI: https://gabormakesgames.com/vectors.html
 
-struct vec3 {
+template<typename T>
+struct TVec3 {
 
 	union {
-		struct { float x, y, z; };
-		float v[3];
+		struct { T x, y, z; };
+		T v[3];
 	};
-	
-	inline vec3()								: x(0.f), y(0.f), z(0.f)		{ }
-	inline vec3(float x_, float y_, float z_)	: x(x_), y(y_), z(z_)			{ }
-	inline vec3(float* fv)						: x(fv[0]), y(fv[1]), z(fv[2])	{ }
-	inline vec3(const vec3& fv)					: x(fv.x), y(fv.y), z(fv.z)		{ }
 
-	inline static vec3 s_zero()		{ return vec3(0,0,0); }
-	inline static vec3 s_one()		{ return vec3(1,1,1); }
+	inline TVec3()					: x(0), y(0), z(0)   { }
+	inline TVec3(const T& x_, const T& y_, const T& z_) : x(x_),y(y_), z(z_) { }
+	inline TVec3(const T* fv)		: x(fv[0]), y(fv[1]), z(fv[2]) { }
+	inline TVec3(const TVec3& fv)	: x(fv.x), y(fv.y), z(fv.z)  { }
+	inline TVec3(const vec2& fv, const T& z_) : x(fv.x), y(fv.y), z(z_) { }
 
-	inline static vec3 s_forward()	{ return vec3( 0, 0, 1); }
-	inline static vec3 s_back()		{ return vec3( 0, 0,-1); }
-	inline static vec3 s_up()		{ return vec3( 0, 1, 0); }
-	inline static vec3 s_down()		{ return vec3( 0,-1, 0); }
-	inline static vec3 s_right()	{ return vec3( 1, 0, 0); }
-	inline static vec3 s_left()		{ return vec3(-1, 0, 0); }
+	inline static TVec3 s_zero()	{ return TVec3(0,0,0); }
+	inline static TVec3 s_one()		{ return TVec3(1,1,1); }
 
-	inline static bool s_equals0(float s) { return s < FLT_EPSILON; }
-	inline static bool s_equals0(const vec3& _v) { return s_equals0(_v.x) && s_equals0(_v.y) && s_equals0(_v.z); }
+	inline static TVec3 s_forward()	{ return TVec3( 0, 0, 1); }
+	inline static TVec3 s_back()	{ return TVec3( 0, 0,-1); }
+	inline static TVec3 s_up()		{ return TVec3( 0, 1, 0); }
+	inline static TVec3 s_down()	{ return TVec3( 0,-1, 0); }
+	inline static TVec3 s_right()	{ return TVec3( 1, 0, 0); }
+	inline static TVec3 s_left()	{ return TVec3(-1, 0, 0); }
 
-	inline vec3 operator+ (const vec3& r) const { return vec3(x + r.x, y + r.y, z + r.z); }
-	inline vec3 operator- (const vec3& r) const { return vec3(x - r.x, y - r.y, z - r.z); }
-	inline vec3 operator* (const vec3& r) const { return vec3(x * r.x, y * r.y, z * r.z); } // non-uniform scaler operator
-	inline vec3 operator/ (const vec3& r) const { return vec3(x / r.x, y / r.y, z / r.z); }
+	inline bool equals(const TVec3& r, const T& epsilon = Math::epsilon<T>()) const;
+	inline bool equals0(               const T& epsilon = Math::epsilon<T>()) const;
 
-	inline void operator+= (const vec3& r) { x += r.x; y += r.y; z += r.z; }
-	inline void operator-= (const vec3& r) { x -= r.x; y -= r.y; z -= r.z; }
-	inline void operator*= (const vec3& r) { x *= r.x; y *= r.y; z *= r.z; }
-	inline void operator/= (const vec3& r) { x /= r.x; y /= r.y; z /= r.z; }
+	inline TVec3 operator+ (const TVec3& r) const { return TVec3(x + r.x, y + r.y, z + r.z); }
+	inline TVec3 operator- (const TVec3& r) const { return TVec3(x - r.x, y - r.y, z - r.z); }
+	inline TVec3 operator* (const TVec3& r) const { return TVec3(x * r.x, y * r.y, z * r.z); } // non-uniform scale operator
+	inline TVec3 operator/ (const TVec3& r) const { return TVec3(x / r.x, y / r.y, z / r.z); }
 
-	inline vec3 operator*	(float s)	const	{ return vec3(x * s, y * s, z * s); }
-	inline vec3 operator/	(float s)	const	{ return vec3(x / s, y / s, z / s); }
-	inline void operator*=	(float s)			{ x *= s; y *= s; z *= s;			}
-	inline void operator/=	(float s)			{ x /= s; y /= s; z /= s;			}
+	inline void operator+= (const TVec3& r) { x += r.x; y += r.y; z += r.z; }
+	inline void operator-= (const TVec3& r) { x -= r.x; y -= r.y; z -= r.z; }
+	inline void operator*= (const TVec3& r) { x *= r.x; y *= r.y; z *= r.z; }
+	inline void operator/= (const TVec3& r) { x /= r.x; y /= r.y; z /= r.z; }
 
-			float& operator[](int i)		{ return v[i]; }
-	const	float& operator[](int i) const	{ return v[i]; }
+	inline TVec3 operator+	(const T& s)	const { return TVec3(x + s, y + s, z + s); }
+	inline TVec3 operator-	(const T& s)	const { return TVec3(x - s, y - s, z - s); }
+	inline TVec3 operator*	(const T& s)	const { return TVec3(x * s, y * s, z * s); }
+	inline TVec3 operator/	(const T& s)	const { return TVec3(x / s, y / s, z / s); }
 
-	inline float dot(const vec3& r)		const { return (x*r.x) + (y*r.y) + (z*r.z); }
-	inline vec3 cross(const vec3& r)	const { return vec3(y*r.z - z*r.y, z*r.x - x*r.z, x*r.y - y*r.x); }
+	inline void operator+=	(const T& s)	{ x += s; y += s; z += s; }
+	inline void operator-=	(const T& s)	{ x -= s; y -= s; z -= s; }
+	inline void operator*=	(const T& s)	{ x *= s; y *= s; z *= s; }
+	inline void operator/=	(const T& s)	{ x /= s; y /= s; z /= s; }
 
-	inline float magnitude()	const { return ::sqrtf(sqrMagnitude()); }
-	inline float sqrMagnitude() const { return dot(*this); }
+	inline TVec3 operator-()				const { return TVec3<T>(-x, -y, -z); }
 
-	inline float lenSq() const { return sqrMagnitude(); }
-	inline float len()	 const { return magnitude(); }
+	inline			T& operator[](int i)		{ return v[i]; }
+	inline const	T& operator[](int i) const	{ return v[i]; }
 
-	inline vec3 normalize() const {
-		float m = magnitude();
-		return s_equals0(m) ? s_zero() : (*this / m);
+	inline	vec2	xy() const { return vec2(x,y); }
+	inline	vec2	xz() const { return vec2(x,z); }
+	inline	vec2	yz() const { return vec2(y,z); }
+
+	inline T		dot(const TVec3& r)		const { return (x*r.x) + (y*r.y) + (z*r.z); }
+	inline TVec3	cross(const TVec3& r)	const { return TVec3(y*r.z - z*r.y, z*r.x - x*r.z, x*r.y - y*r.x); }
+
+	inline T	magnitude()		const { return Math::sqrt(sqrMagnitude()); }
+	inline T	sqrMagnitude()	const { return dot(*this); }
+
+	inline T	lenSq()			const { return sqrMagnitude(); }
+	inline T	len()			const { return magnitude(); }
+
+	inline TVec3 normalize() const {
+		T m = magnitude();
+		return Math::equals0(m) ? s_zero() : (*this / m);
 	}
 
 	// radians
-	inline float angle(const vec3& r) const {
-		float m1 = magnitude();
-		if (s_equals0(m1)) return 0;
+	inline T angle(const TVec3& r) const {
+		T m1 = magnitude();
+		if (Math::equals0(m1)) return 0;
 
-		float m2 = r.magnitude();
-		if (s_equals0(m2)) return 0;
+		T m2 = r.magnitude();
+		if (Math::equals0(m2)) return 0;
 
-		float cos_theta = dot(r) / (m1 * m2);
-		return ::acosf(cos_theta);
+		T cos_theta = dot(r) / (m1 * m2);
+		return Math::acos(cos_theta);
 	}
 
 	// project *this onto r
-	inline vec3 project(const vec3& r) const {
-		float m = r.magnitude();
-		if (s_equals0(m)) return s_zero();
+	inline TVec3 project(const TVec3& r) const {
+		T m = r.magnitude();
+		if (Math::equals0(m)) return s_zero();
 
 		// *this is hypotenuse
 		// so, s = ||hypotenuse|| * cos(theta) = dot(r) / ||r||
 
-		float s = dot(r) / m;
+		T s = dot(r) / m;
 		return r * s;
 	}
 
 	// rejection is perpendicular to projection
-	inline vec3 reject(const vec3& r) const {
-		vec3 proj = project(r);
+	inline TVec3 reject(const TVec3& r) const {
+		TVec3 proj = project(r);
 		return *this - proj;
 	}
 
 	// bounce-like reflect
-	inline vec3 reflect(const vec3& r) const {
-		vec3 proj = project(r);
-		if (s_equals0(proj)) return s_zero();
+	inline TVec3 reflect(const TVec3& r) const {
+		TVec3 proj = project(r);
+		if (proj.equals0()) return TVec3::s_zero();
 
-		vec3 projx2 = proj+proj;
+		TVec3 projx2 = proj+proj;
 		return *this - projx2;
 	}
 
 	// linear interpolatation
-	inline vec3 lerp(const vec3& to, float t) const {
-		// return *this*(1.0f-t) + to*t;
+	inline TVec3 lerp(const TVec3& to, const T& t) const {
+//		return *this*(1.0f-t) + to*t;
+//		return *this + (to - *this)*t;
+//		return TVec3(x + ( (to.x-x)*t ), y + ( (to.y-y)*t ), z + ( (to.z-z)*t ));
 
-		/*
-			return vec3(
-				x + ( (to.x-x)*t ),
-				y + ( (to.y-y)*t ),
-				z + ( (to.z-z)*t )
-			);
-		*/
-		return *this + (to - *this)*t;
+		return TVec3(Math::lerp(x, to.x, t),
+					Math::lerp(y, to.y, t),
+					Math::lerp(z, to.z, t));
 	}
 
 	// spherical linear interpolatation: constant in velocity
-	inline vec3 slerp(const vec3& to_, float t) const {
+	inline TVec3 slerp(const TVec3& to_, const T& t) const {
 
 		// when t close to 0, slerp will yield unexpected results
-		if (s_equals0(::abs(t-0.01f))) return lerp(to_, t);
+		if (Math::equals0(Math::abs(t - 0.01))) {
+			return lerp(to_, t);
+		}
 
-		vec3 from = this->normalize();
-		vec3 to   = to_.normalize();
+		TVec3 from = this->normalize();
+		TVec3 to   = to_.normalize();
 
-		float theta = from.angle(to);
-		float sin_theta = ::sinf(theta);
+		T theta		= from.angle(to);
+		T sin_theta = Math::sin(theta);
 
-		float a = ::sinf( (1.0f-t)*theta ) / sin_theta;
-		float b = ::sinf(t*theta) / sin_theta;
+		T a = Math::sin( (1.0f-t)*theta ) / sin_theta;
+		T b = Math::sin(t*theta) / sin_theta;
 
 		return (from*a) + (to*b);
 	}
 
 	// normalize lerp: non constant in velocity
-	inline vec3 nlerp(const vec3& to, float t) const { return lerp(to, t).normalize(); }
+	inline TVec3 nlerp(const TVec3& to, const T& t) const { return lerp(to, t).normalize(); }
 
-	inline bool operator== (const vec3& r) const { return s_equals0(*this - r); }
-	inline bool operator!= (const vec3& r) const { return !(*this == r); }
+	inline bool operator== (const TVec3& r) const { return equals(r); }
+	inline bool operator!= (const TVec3& r) const { return !(this->operator==(r)); }
 };
 
-inline float	dot  (const vec3& l, const vec3& r)					{ return l.dot(r); }
-inline vec3		cross(const vec3& l, const vec3& r)					{ return l.cross(r); }
-inline float	lenSq(const vec3& v)								{ return v.sqrMagnitude(); }
-inline float	len  (const vec3& v)								{ return v.magnitude(); }
-inline vec3		normalize(const vec3& v)							{ return v.normalize(); }
-inline float	angle  (const vec3& l, const vec3& r)				{ return l.angle(r); }
-inline vec3		project(const vec3& l, const vec3& r)				{ return l.project(r); }
-inline vec3		reject (const vec3& l, const vec3& r)				{ return l.reject(r); }
-inline vec3		reflect(const vec3& l, const vec3& r)				{ return l.reflect(r); }
-inline vec3		lerp (const vec3& from, const vec3& to, float t)	{ return from.lerp(to, t); }
-inline vec3		slerp(const vec3& from, const vec3& to, float t)	{ return from.slerp(to, t); }
-inline vec3		nlerp(const vec3& from, const vec3& to, float t)	{ return from.nlerp(to, t); }
+template<class T> inline
+bool TVec3<T>::equals(const TVec3& r, const T& epsilon) const {
+	return Math::equals(x, r.x, epsilon)
+		&& Math::equals(y, r.y, epsilon)
+		&& Math::equals(z, r.z, epsilon);
+}
+
+template<class T> inline
+bool TVec3<T>::equals0(const T& epsilon) const {
+	return Math::equals0(x, epsilon)
+		&& Math::equals0(y, epsilon)
+		&& Math::equals0(z, epsilon);
+}
+
+template<>
+struct TVec3<int> {
+	using T = int;
+	using This = TVec3<T>;
+	union {
+		struct { T x, y, z; };
+		T v[3];
+	};
+
+	inline TVec3(T x_, T y_, T z_) : x(x_), y(y_), z(z_) { }
+};
+
+template<>
+struct TVec3<unsigned int> {
+	using T = unsigned int;
+	using This = TVec3<T>;
+	union {
+		struct { T x, y, z; };
+		T v[3];
+	};
+
+	inline TVec3(T x_, T y_, T z_) : x(x_), y(y_), z(z_) { }
+};
+
+using vec3		= TVec3<float>;
+using ivec3		= TVec3<int>;
+using uivec3	= TVec3<unsigned int>;
 
 }
